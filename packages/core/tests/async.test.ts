13c414fe901afce3c39857c63b72d665417a69c6
Codefoxdev <admin@codefoxdev.com>
2025-09-03 17:45:54 +0200

    Started implementing tests for the AsyncResult class

---
+++ packages/core/tests/async.test.ts
@@ -0,0 +1,28 @@
+import { describe, expect, test, vi } from "vitest";
+import { AsyncResult, Err, Ok, Result } from "../src/index";
+
+describe("AsyncResult constructors", () => {
+  describe("Ok constructor", () => {
+    test("Should create an AsyncResult instance that resolves to an Ok result", async () => {
+      const asyncOk = AsyncResult.Ok(42);
+      expect(asyncOk).toBeInstanceOf(AsyncResult);
+
+      const awaited = await asyncOk;
+      expect(awaited).toBeInstanceOf(Result.Ok);
+      expect(awaited.ok).toBe(true);
+      expect(awaited._val).toBe(42);
+    });
+  });
+
+  describe("Err constructor", () => {
+    test("Should create an AsyncResult instance that resolves to an Err result", async () => {
+      const asyncOk = AsyncResult.Err("Uh oh!");
+      expect(asyncOk).toBeInstanceOf(AsyncResult);
+
+      const awaited = await asyncOk;
+      expect(awaited).toBeInstanceOf(Result.Err);
+      expect(awaited.err).toBe(true);
+      expect(awaited._val).toBe("Uh oh!");
+    });
+  });
+});
